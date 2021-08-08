a0d138435c51cabfd2668378e1606dc60d5b54fb
SergeiPozharkov <74614672+SergeiPozharkov@users.noreply.github.com>
2021-08-08 21:47:12 +0300

    Update add.php

--- templates/UserShowAddOrganisation/add.php
+++ templates/UserShowAddOrganisation/add.php
@@ -22,6 +22,7 @@
                             echo $value . " * <br>";
                             echo "<span>Дни недели:</span>" . $select = <<<SELECT
 <select name="week_days">
+  <option value="" style="display:none;"></option>
   <option value="Пн.-Вс.:">Пн.-Вс.:</option>
   <option value="Пн.-Пт.:">Пн.-Пт.:</option>
   <option value="Пн.-Сб.:">Пн.-Сб.:</option>
