c8f460e6ee26b9ef359c91073098d5756c7dcd57
SergeiPozharkov <74614672+SergeiPozharkov@users.noreply.github.com>
2021-08-12 14:41:27 +0300

    delete comments code from user menu

--- templates/menu_user.php
+++ templates/menu_user.php
@@ -1,36 +1,3 @@
-<!--<nav class="navbar navbar-expand-lg navbar-dark bg-dark">-->
-<!--    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"-->
-<!--            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
-<!--        <span class="navbar-toggler-icon"></span>-->
-<!--    </button>-->
-<!---->
-<!--    <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
-<!--        <ul class="navbar-nav mr-auto">-->
-<!--            <li class="nav-item active">-->
-<!--                <a class="nav-link--><? //= $this->data['controllerName'] == "MainController" ? " active" : "" ?><!--" href="?">Главная</a>-->
-<!--            </li>-->
-<!--            <li class="nav-item">-->
-<!--                <a class="nav-link--><? //= $this->data['controllerName'] == "UserShowReviewsController" ? " active" : "" ?><!--"-->
-<!--                   href="?type=UserShowReviews&action=show">Отзывы</a>-->
-<!--            </li>-->
-<!--            <li class="nav-item">-->
-<!--                <a class="nav-link--><? //= $this->data['controllerName'] == "UserShowAddOrganisationController" ? " active" : "" ?><!--"-->
-<!--                   href="?type=UserShowAddOrganisation&action=showadd">Добавить организацию</a>-->
-<!--            </li>-->
-<!--            <li class="nav-item">-->
-<!--                <a class="nav-link--><? //= $this->data['controllerName'] == "UserShowAddReviewController" ? " active" : "" ?><!--"-->
-<!--                   href="?type=UserShowAddReview&action=showadd">Добавить отзыв</a>-->
-<!--            </li>-->
-<!--            <li class="nav-item">-->
-<!--                <a class="nav-link"-->
-<!--                   href="?type=Aut&action=logout">Выйти(--><? //= isset($_SESSION['user']['code']) ? $_SESSION['user']['name'] : "" ?>
-<!--                    )-->
-<!--                </a>-->
-<!--            </li>-->
-<!--        </ul>-->
-<!--    </div>-->
-<!--</nav>-->
-
 <!------->
 <div id="rotate_logo">
     <h1>
