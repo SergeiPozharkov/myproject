d4afb1d414b680e725371110416488cf84faa3f0
SergeiPozharkov <74614672+SergeiPozharkov@users.noreply.github.com>
2021-08-12 15:37:21 +0300

    update templates gest, main and css

--- templates/GuestShowReviews/show.php
+++ templates/GuestShowReviews/show.php
@@ -1,20 +1,33 @@
-<?php
+<div class="container">
+    <div class="row">
+        <div class="col">
+        </div>
+        <div class="col">
 
-use W1020\HTML\Pagination;
-use W1020\HTML\Table;
+            <div id="reviews">
+                <?php
 
-//print_r($this->data);
-echo "<table>";
-foreach ($this->data['table'] as $key => $row) {
-    echo "<tr><td><a href='?type={$this->data['controllerName']}&action=showOrganisationReviews&id=$row[organisation_id]'><b>$row[title]</b></a></tr></td>";
-    echo "<tr><td><b>$row[users_name]</b></td></tr>";
-    echo "<tr><td><b>$row[date]</b></td></tr>";
-    echo "<tr><td><a href='?type={$this->data['controllerName']}&action=showReview&id=$row[id]' class='btn btn-primary'>Подробнее</a></td></tr>";
-}
-echo "</table>";
-echo (new Pagination())
-    ->setUrlPrefix("?type={$this->data['controllerName']}&action=show&page=")
-    ->setPageCount($this->data["pageCount"])
-    ->setActivePage($this->data["activePage"])
-    ->html();
-?>
+                use W1020\HTML\Pagination;
+                use W1020\HTML\Table;
+
+                //print_r($this->data);
+                echo "<table>";
+                foreach ($this->data['table'] as $key => $row) {
+                    echo "<tr><td><a href='?type={$this->data['controllerName']}&action=showOrganisationReviews&id=$row[organisation_id]'><b>$row[title]</b></a></tr></td>";
+                    echo "<tr><td><b>$row[users_name]</b></td></tr>";
+                    echo "<tr><td><b>$row[date]</b></td></tr>";
+                    echo "<tr><td><a href='?type={$this->data['controllerName']}&action=showReview&id=$row[id]' class='btn btn-primary'>Подробнее</a></td></tr>";
+                }
+                echo "</table>";
+                echo (new Pagination())
+                    ->setUrlPrefix("?type={$this->data['controllerName']}&action=show&page=")
+                    ->setPageCount($this->data["pageCount"])
+                    ->setActivePage($this->data["activePage"])
+                    ->html();
+                ?>
+            </div>
+        </div>
+        <div class="col">
+        </div>
+    </div>
+</div>
