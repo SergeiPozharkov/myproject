406ed158baa79091adebd6e580e39f6b3c3525dd
SergeiPozharkov <74614672+SergeiPozharkov@users.noreply.github.com>
2021-08-12 15:21:46 +0300

    update form login, registration and css file

--- templates/Aut/login.php
+++ templates/Aut/login.php
@@ -8,7 +8,9 @@
                 <form action="?type=Aut&action=login" method="post">
                     <input class="form-control form-group" type="text" name="login" placeholder="Введите логин">
                     <input class="form-control form-group" type="password" name="pass" placeholder="Введите пароль">
-                    <input class="btn btn-lg btn-primary btn-block btn-signin" type="submit" id="button" value="OK">
+                    <div id="button_login">
+                        <button class="btn btn-lg btn-success btn-block btn-signin" type="submit">Войти</button>
+                    </div>
                 </form>
             </div>
         </div>
--- templates/Aut/registration.php
+++ templates/Aut/registration.php
@@ -1,15 +1,33 @@
-<form action="?type=Aut&action=reg" method="post">
-    <input class="form-control form-group" type="text" name="name" placeholder="Введите имя"
-           value="<?= $_SESSION['regData']['name'] ?? '' ?>">
-    <input class="form-control form-group" type="text" name="login" placeholder="Введите логин"
-           value="<?= $_SESSION['regData']['login'] ?? '' ?>">
-    <input class="form-control form-group" type="password" name="pass1" placeholder="Введите пароль"
-           value="<?= $_SESSION['regData']['pass1'] ?? '' ?>">
-    <input class="form-control form-group" type="password" name="pass2" placeholder="Введите пароль повторно"
-           value="<?= $_SESSION['regData']['pass2'] ?? '' ?>">
+<div class="container">
+    <div class="row">
+        <div class="col">
+        </div>
+        <div class="col">
+            <div id="reg_form">
+                <h1 id="form_title">Форма регистрации:</h1>
+                <form action="?type=Aut&action=reg" method="post">
+                    <input class="form-control form-group" type="text" name="name" placeholder="Введите имя"
+                           value="<?= $_SESSION['regData']['name'] ?? '' ?>">
+                    <input class="form-control form-group" type="text" name="login" placeholder="Введите логин"
+                           value="<?= $_SESSION['regData']['login'] ?? '' ?>">
+                    <input class="form-control form-group" type="password" name="pass1" placeholder="Введите пароль"
+                           value="<?= $_SESSION['regData']['pass1'] ?? '' ?>">
+                    <input class="form-control form-group" type="password" name="pass2"
+                           placeholder="Введите пароль повторно"
+                           value="<?= $_SESSION['regData']['pass2'] ?? '' ?>">
+                    <div id="button_reg">
+                        <button class="btn btn-lg btn-success btn-block btn-signin" type="submit"
+                        >Зарегистрироваться
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="col">
+        </div>
+    </div>
+</div>
 
-    <input class="btn btn-lg btn-primary btn-block btn-signin" type="submit" value="OK">
-</form>
 <?php
 unset($_SESSION['regData']);
 ?>
